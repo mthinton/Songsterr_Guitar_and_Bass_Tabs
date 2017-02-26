af2fe32d89ddd61be2b5e29c3fa110b6a467c7c4
mthintoncoding <mthinton@gmail.com>
2017-02-26 12:27:13 -0500

    app functioning in very basic format

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-var songsterr_Endpoint_URL = 'https://www.songsterr.com/a/rest/';
+var songsterr_Endpoint_URL = 'https://www.songsterr.com/a/ra/songs/byartists.json';
 
 function getDataFromSongsterr (artistName, callback){
 	var query = {
-		a: 'artistName',
+		artists: artistName
 	}
 	$.getJSON(songsterr_Endpoint_URL, query, callback)
 }
 
 function displaySearchResults(data){
 	var resultElement = '';
-	if(data.items){
-		data.items.forEach(function(item){
-			resultElement += '<a href="https://www.songsterr.com/a/wa/artist?id='+item.id+'"></a>'
+	if(data){
+		data.forEach(function(item){
+			resultElement += '<a href="https://www.songsterr.com/a/wa/song?id='+item.id+'"target="_blank">'+item.title+' <br><br> </a>'
+			console.log(resultElement);
 		});
 	}
 	else{
 		resultElement += '<p>No results </p>'
 	}
-	$('results_section').html(resultElement);
+	$('.results_section').html(resultElement);
 }
 
 function eventHandler(){
@@ -25,7 +26,6 @@ function eventHandler(){
 		e.preventDefault();
 		var query = $(this).find('#artist_name').val();
 		getDataFromSongsterr(query, displaySearchResults);
-		console.log($('#artist_name'));
 	});
 }
 
