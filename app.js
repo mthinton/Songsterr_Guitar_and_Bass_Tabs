a944401e5cb65a963807ea2b9a6f7fb763f86747
mthintoncoding <mthinton@gmail.com>
2017-03-02 16:40:42 -0500

    final commit for Thinkful review

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
-var songsterr_Endpoint_URL = 'https://www.songsterr.com/a/ra/songs/byartists.json';
+var songsterr_Endpoint_URL = 'http://www.songsterr.com/a/ra/songs.json';
 
 function getDataFromSongsterr (artistName, callback){
 	var query = {
-		artists: artistName
+		pattern: artistName
 	}
 	$.getJSON(songsterr_Endpoint_URL, query, callback)
 }
 
-function displaySearchResults(data){
+function displaySearchResults(data){//
 	var resultElement = '';
 	if(data){
 		data.forEach(function(item){
 			resultElement += '<a href="https://www.songsterr.com/a/wa/song?id='+item.id+'"target="_blank">'+item.title+' <br><br> </a>'
-			console.log(resultElement);
 		});
 	}
 	else{
-		resultElement += '<p>No results </p>'
+		resultElement += '<p> No results </p>'
 	}
 	$('.results_section').html(resultElement);
 }
 
-function eventHandler(){
+function eventHandler2(){
 	$('.search_Songsterr').submit(function(e){
 		e.preventDefault();
 		var query = $(this).find('#artist_name').val();
@@ -29,4 +28,6 @@ function eventHandler(){
 	});
 }
 
-$(function(){eventHandler();});
+$(function(){;eventHandler2();});
+
+
