e6e9adb5e9f459f7c746023a21408d6aa5b05ad7
mthintoncoding <mthinton@gmail.com>
2017-03-02 17:33:42 -0500

    Edited unused code and added comments

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var songsterr_Endpoint_URL = 'http://www.songsterr.com/a/ra/songs.json';
 
-function getDataFromSongsterr (artistName, callback){
+function getDataFromSongsterr (artistName, callback){//This is the request for songs from a particular artist
 	var query = {
 		pattern: artistName
 	}
 	$.getJSON(songsterr_Endpoint_URL, query, callback)
 }
 
-function displaySearchResults(data){//
+function displaySearchResults(data){//This displays to the div 'results_sections'
 	var resultElement = '';
 	if(data){
 		data.forEach(function(item){
@@ -20,7 +20,7 @@ function displaySearchResults(data){//
 	$('.results_section').html(resultElement);
 }
 
-function eventHandler2(){
+function eventHandler2(){//this gets the artist name, turns it into a string, and we pass this into getDataFromSongsterr
 	$('.search_Songsterr').submit(function(e){
 		e.preventDefault();
 		var query = $(this).find('#artist_name').val();
